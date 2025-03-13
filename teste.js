6bd1b5fc5d39f76a5489802c062e5ae2fa2945e6
Kekalbe <rodrigoreiss2005@gmail.com>
2025-03-12 23:25:39 -0300

    Adicionando Código Teste

---
+++ teste.js
@@ -0,0 +1,19 @@
+const personagens = [
+    {
+      clicar: document.querySelector('.viviana-p'),
+      novoTextoName: 'Viviana',
+          imagem: 'Personagens Seletor\Viviana_Elite_2.webp',
+      elementos: {
+        name: document.querySelector('.name'),
+              imagemElemento: document.querySelector('.imagem')
+  }
+      },
+      {
+          clicar: document.querySelector('.amiya-p'),
+      novoTextoName: 'Amiya',
+          imagem: 'Personagens\Amiya_(Medic).webp',
+      elementos: {
+        name: document.querySelector('.name'),
+              imagemElemento: document.querySelector('.imagem')
+      }
+      }];
