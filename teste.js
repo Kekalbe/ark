226a2861c4d782cb09975f50d29bc6bad948a740
Kekalbe <rodrigoreiss2005@gmail.com>
2025-03-14 18:14:58 -0300

    teste 3

--- teste.js
+++ teste.js
@@ -1,29 +1,40 @@
-const personagens = [
-    {
-      clicar: document.querySelector('.viviana-p'),
-      novoTextoName: 'Viviana',
-          imagem: 'Personagens Seletor\Viviana_Elite_2.webp',
-      elementos: {
-        name: document.querySelector('.name'),
-              imagemElemento: document.querySelector('.imagem')
-  }
-      },
-      {
-          clicar: document.querySelector('.amiya-p'),
-      novoTextoName: 'Amiya',
-          imagem: 'Personagens Seletor\Amiya_(Medic).webp',
-      elementos: {
-        name: document.querySelector('.name'),
-              imagemElemento: document.querySelector('.imagem')
-      }
-      },
-      {
-        clicar: document.querySelector('.civilight-p'),
-    novoTextoName: 'Civilight Eterna',
-        imagem: 'Personagens Seletor\Civilight_Eterna_Elite_2.webp',
-    elementos: {
-      name: document.querySelector('.name'),
-            imagemElemento: document.querySelector('.imagem')
-    }
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    const personagens = [
+        {
+            clicar: document.querySelector('.viviana-p'),
+            novoTextoName: 'Viviana',
+            imagem: 'Personagens Seletor\\Viviana_Elite_2.webp',
+            elementos: {
+                name: document.querySelector('.name'),
+                imagemElemento: document.querySelector('.imagem')
+            }
+        },
+        {
+            clicar: document.querySelector('.amiya-p'),
+            novoTextoName: 'Amiya',
+            imagem: 'Personagens Seletor\\Amiya_(Medic).webp',
+            elementos: {
+                name: document.querySelector('.name'),
+                imagemElemento: document.querySelector('.imagem')
+            }
+        },
+        {
+            clicar: document.querySelector('.civilight-p'),
+            novoTextoName: 'Civilight Eterna',
+            imagem: 'Personagens Seletor\\Civilight_Eterna_Elite_2.webp',
+            elementos: {
+                name: document.querySelector('.name'),
+                imagemElemento: document.querySelector('.imagem')
+            }
+        }
     ];
+
+    // Coloque o restante do seu código aqui
+    personagens.forEach(function(personagem) {
+        personagem.clicar.addEventListener('click', function() {
+            // Alterando os textos e outros dados
+            personagem.elementos.name.textContent = personagem.novoTextoName;
+            personagem.elementos.imagemElemento.src = personagem.imagem;
+        });
+    });
+});
