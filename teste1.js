129933106c6e0eb0cafa49335f1dddc96f6dfb33
Kekalbe <rodrigoreiss2005@gmail.com>
2025-03-15 00:11:32 -0300

    Personagens

--- teste1.js
+++ teste1.js
@@ -11,7 +11,7 @@ const personagens = [
         {
             clicar: document.querySelector('.amiya-p'),
             novoTextoName: 'Amiya',
-            imagem: 'https://ark-1qo.pages.dev/PersonagensSeletor/Amiya_(Medic).webp',
+            imagem: 'https://raw.githubusercontent.com/Kekalbe/ark/refs/heads/master/PersonagensSeletor/Amiya%20Medic/Amiya_(Medic).webp',
             elementos: {
                 name: document.querySelector('.name'),
                 imagemElemento: document.querySelector('.imagem-p')
