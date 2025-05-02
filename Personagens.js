61543efe931a2a91060895bb81093bbb2aa6346a
Kekalbe <rodrigoreiss2005@gmail.com>
2025-05-02 20:35:21 -0300

    Nova Personagem Adicionada Virtuosa

--- Personagens.js
+++ Personagens.js
@@ -18,7 +18,17 @@ const personagens = [
             name: document.querySelector('.name'),
             imagemElemento: document.querySelector('.imagem-p img')
         }
+    },
+    {
+        clicar: document.querySelector('.virtuosa-p'),
+        novoTextoName: 'Virtuosa',
+        imagem: 'https://cdn.jsdelivr.net/gh/Kekalbe/ark@3c715e96b191769610cb113e9c5e442b9a7d9c1a/PersonagensSeletor/__reed_arknights_drawn_by_vxdrq__2e911319857ddcc9967db0cf682c939c-removebg-preview.png',
+        elementos: {
+            name: document.querySelector('.name'),
+            imagemElemento: document.querySelector('.imagem-p img')
+        }
     }
+
 ];
 
 export default personagens;
