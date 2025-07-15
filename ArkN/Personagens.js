14fa367516054eddeff7ef9dfc69de8330c8323e
Kekalbe <rodrigoreiss2005@gmail.com>
2025-07-14 23:09:49 -0300

    T

--- ArkN/Personagens.js
+++ ArkN/Personagens.js
@@ -102,3 +102,4 @@ const personagens = [
     }
 ];
 
+export default personagens;
--- ArkN/teste1.js
+++ ArkN/teste1.js
@@ -1,5 +1,5 @@
 // main.js
-
+import personagens from './Personagens.js';
 
 personagens.forEach(function(personagem) {
     if (!personagem.clicar) return;
