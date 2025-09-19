e3c5e63c1b77e167a735bd0d9f15354a4bfc967c
Kekalbe <rodrigoreiss2005@gmail.com>
2025-09-19 19:54:38 -0300

    Testing

--- ArkN/vite.config.js
+++ ArkN/vite.config.js
@@ -1,30 +1,21 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  root: 'src', // pasta raiz do projeto
-  publicDir: '../public', // garante que tudo em public vá para dist
+  root: 'src',
+  publicDir: '../public', // copia tudo que estiver em public para dist
   build: {
-    outDir: '../dist', // pasta final de build
+    outDir: '../dist',
     emptyOutDir: true,
-    cssCodeSplit: true, // CSS vai para arquivo separado
+    cssCodeSplit: true, // gera arquivo CSS separado
     rollupOptions: {
-      input: resolve(__dirname, 'src/js/main.js'), // arquivo de entrada absoluto
+      input: {
+        main: 'src/js/main.js',      // entrada JS
+        style: 'src/css/main.css'    // entrada CSS
+      },
       output: {
-        entryFileNames: '[name].[hash].js', // nome dos JS
-        assetFileNames: '[name].[hash].[ext]' // nome dos assets (CSS, imagens)
+        entryFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
       }
     }
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../public/*', // tudo da pasta public
-          dest: '.' // copiar direto para dist
-        }
-      ]
-    })
-  ]
+  }
 });
